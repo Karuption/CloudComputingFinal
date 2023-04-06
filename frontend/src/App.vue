08a9c3f50d687d1af13f61895b28ff9fb50a6210
servosdesign <servos.design0@gmail.com>
2023-04-06 13:27:39 -0500

    frontend start, lint, font import, favicon

---
+++ frontend/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    dd
+  </div>
+</template>
+
+<script>
+/*
+    Todo:
+*/
+
+export default {
+  data  () {
+    return { }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Poppins&display=swap');
+
+body {
+  background-color: rgb(251, 251, 251);
+}
+
+body, html {
+    margin: 0;
+    padding: 0;
+}
+
+* {
+  font-family: Poppins;
+}
+
+</style>
