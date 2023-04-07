78e330edb14e8b1fd8d67a821c39b1235666f6d2
Karuption <pederson26@gmail.com>
2023-04-07 15:59:57 -0500

    Dockerfile in the root dir

---
+++ CCFinal/CCFinal/Controllers/ToDoIntegrationController.cs
@@ -0,0 +1,116 @@
+﻿using CCFinal.Data;
+using CCFinal.Dtos;
+using CCFinal.Mappers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CCFinal.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ToDoIntegrationController : ControllerBase {
+    private readonly CCFinalContext _context;
+    private readonly ITodoMapper _todoMapper;
+
+    public ToDoIntegrationController(CCFinalContext context, ITodoMapper todoMapper) {
+        _context = context;
+        _todoMapper = todoMapper;
+    }
+
+    // GET: api/ToDoTask
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<IEnumerable<ToDoTaskIntegrationDto>>> GetToDoTask() {
+        if (_context.ToDoTask == null)
+            return NotFound();
+
+        return _context.ToDoTask.Select(x => _todoMapper.TodoTaskModelToDto(x)).ToList();
+    }
+
+    // GET: api/ToDoTask/5
+    [HttpGet("{id}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<ToDoTaskIntegrationDto>> GetToDoTask(string id) {
+        if (_context.ToDoTask == null)
+            return NotFound();
+        var toDoTask = await _context.ToDoTask.FirstOrDefaultAsync(x => x.IntegrationId == id);
+
+        if (toDoTask == null)
+            return NotFound();
+
+        return _todoMapper.TodoTaskModelToDto(toDoTask);
+    }
+
+    // PUT: api/ToDoTask/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> PutToDoTask(string id, ToDoTaskIntegrationDto toDoTaskDTO) {
+        var toDoTask = _todoMapper.TodoTaskDtoToModel(toDoTaskDTO);
+
+        if (id != toDoTask.IntegrationId)
+            return BadRequest();
+
+        var task = _context.ToDoTask.FirstOrDefault(x => x.IntegrationId == id);
+
+        toDoTask.Id = task.Id;
+
+        _context.Entry(toDoTask).State = EntityState.Modified;
+
+        try {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException) {
+            if (!ToDoTaskExists(task.Id))
+                return NotFound();
+            throw;
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/ToDoTask
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<ToDoTaskDTO>> PostToDoTask(ToDoTaskDTO toDoTaskDto) {
+        var toDoTask = new TodoMapper().TodoTaskDtoToModel(toDoTaskDto);
+        toDoTask.Created = DateTime.UtcNow;
+
+        if (_context.ToDoTask == null)
+            return Problem("Entity set 'CCFinalContext.ToDoTask'  is null.");
+
+        _context.ToDoTask.Add(toDoTask);
+        await _context.SaveChangesAsync();
+
+        toDoTaskDto = _todoMapper.TodoTaskToDto(toDoTask);
+
+
+        return CreatedAtAction("GetToDoTask", new { id = toDoTaskDto.Id }, toDoTaskDto);
+    }
+
+    // DELETE: api/ToDoTask/5
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    public async Task<IActionResult> DeleteToDoTask(int id) {
+        if (_context.ToDoTask == null) return NotFound();
+        var toDoTask = await _context.ToDoTask.FindAsync(id);
+        if (toDoTask == null) return NotFound();
+
+        _context.ToDoTask.Remove(toDoTask);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool ToDoTaskExists(int id) {
+        return (_context.ToDoTask?.Any(e => e.Id == id)).GetValueOrDefault();
+    }
+}
