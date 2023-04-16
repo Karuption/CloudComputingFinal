b23e521148204fcb220e5121ef5fd53ec6326c1a
Karuption <pederson26@gmail.com>
2023-04-16 00:50:01 -0500

    Worker Service is able to query the Canvas API.

--- CCFinal/CCFinal.CanvasIntegration/Program.cs
+++ CCFinal/CCFinal.CanvasIntegration/Program.cs
@@ -1,9 +1,23 @@
+using System.Net.Http.Headers;
 using CCFinal.CanvasIntegration;
 
 var host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => {
+    .ConfigureAppConfiguration((hostBuilderContext, config) => {
+        if (!hostBuilderContext.HostingEnvironment.IsDevelopment())
+            config.AddEnvironmentVariables("KEY__");
+    })
+    .ConfigureServices((hostContext, services) => {
         services.AddHostedService<Worker>();
         services.AddLogging();
+        services.AddSingleton<HttpClient>(_ => {
+            var client = new HttpClient();
+            var uri = hostContext.Configuration.GetSection("BaseUris")["Canvas"] ?? string.Empty;
+            client.BaseAddress = new Uri(uri);
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer",
+                hostContext.Configuration.GetSection("ApiKeys")["Canvas"]);
+
+            return client;
+        });
     })
     .Build();
 
--- CCFinal/CCFinal.CanvasIntegration/Worker.cs
+++ CCFinal/CCFinal.CanvasIntegration/Worker.cs
@@ -2,18 +2,25 @@ namespace CCFinal.CanvasIntegration;
 
 public class Worker : BackgroundService {
     private readonly ILogger<Worker> _logger;
+    private readonly HttpClient _client;
 
-    public Worker(ILogger<Worker> logger) {
+    public Worker(ILogger<Worker> logger, HttpClient client) {
         _logger = logger;
+        _client = client;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
         _logger.LogInformation("Worker Started");
 
-        while (!stoppingToken.IsCancellationRequested) {
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
-        }
+        var response = await _client.GetAsync("courses", stoppingToken);
+
+        _logger.LogCritical($"Status code: {response.StatusCode.ToString()}");
+        _logger.LogCritical($"Status code: {await response.Content.ReadAsStringAsync(stoppingToken)}");
+
+        //while (!stoppingToken.IsCancellationRequested) {
+        //    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+        //    await Task.Delay(1000, stoppingToken);
+        //}
 
         if (stoppingToken.IsCancellationRequested)
             _logger.LogInformation("Cancellation requested");
